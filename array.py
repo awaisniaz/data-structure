9eb24e2aafcb987cbe337fa70518ec30e1428c2c
awaisniaz <awaisniaz1995@gmail.com>
2026-08-23 11:47:29 +0500

    feat: remove duplicate

--- array.py
+++ array.py
@@ -1,4 +1,18 @@
-def findSumOfPair(arr):
-    print(arr)
+def findAndBuy(arr):
+    min = arr[0]
+    index = 0
+    maxProfit = 0
+    for i in range(len(arr)):
+        if arr[i] < min:
+            min = arr[i]
+            index = i
+    for j in range(index+1,len(arr)):
+        currentProfit = arr[j] - min
+        if currentProfit > maxProfit:
+            maxProfit = currentProfit
+    return maxProfit
 
-print(findSumOfPair([2, 7, 11, 15],9))
+
+
+
+print(findAndBuy([7, 1, 5, 3, 6, 4]))
